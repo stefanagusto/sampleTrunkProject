909baaeddabbfb1f48a74a6aa5ea93c6dd6ee85a
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2024-07-12 09:51:35 +0700

    Initial Commit

---
+++ sampleTrunkProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sampleTrunkProject
+//
+//  Created by Stefan Agusto Hutapea on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sampleTrunkProject/sampleTrunkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleTrunkProjectApp.swift
+//  sampleTrunkProject
+//
+//  Created by Stefan Agusto Hutapea on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct sampleTrunkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
